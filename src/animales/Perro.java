fbe1685018564cd9d51d0e7514c35f11684ef6d0
Elio1991 <127357766+Elio1991@users.noreply.github.com>
2023-03-26 18:32:17 -0300

    modifique el codigo

--- src/animales/Perro.java
+++ src/animales/Perro.java
@@ -13,7 +13,7 @@ public class Perro {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // modifique esto
     }
     
 }
